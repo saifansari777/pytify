dc25070fe04aaf296acc131ecf3f607fa0973577
Saif Ansari <saifansari.ansari8@gmail.com>
2020-03-25 11:22:48 +0530

    intial commit

---
+++ musicterminal/musicterminal/client/app.py
@@ -0,0 +1,8 @@
+import curses
+import curses.panel
+from curses import wrapper
+from curses.textpad import Textbox
+from curses.textpad import rectangle
+
+from client import Menu
+from client import DataManager
---
+++ musicterminal/musicterminal/pytify/auth/auth_method.py
@@ -0,0 +1,7 @@
+from enum import Enum, auto
+
+
+class AuthMethod(Enum):
+	CLIENT_CREDENTIALS = auto()
+	AUTHORIZATION_CODE = auto()
+	
